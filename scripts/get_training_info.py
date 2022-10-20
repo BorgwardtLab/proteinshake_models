d42d0a670461833e4ad9aecc0fdf5318be3becc7
Carlos G. Oliver <carlos.gonzalez.oliver@gmail.com>
2022-10-20 14:22:43 +0200

    works with 0.0.3

--- scripts/get_training_info.py
+++ scripts/get_training_info.py
@@ -19,14 +19,16 @@ def compute_splits(dataset,
                    validation_ratio=.15,
                    test_ratio=.10):
 
-    inds = range(len(list(dataset.proteins)))
+    _, size = dataset.proteins()
+    inds = range(size)
     train, test = train_test_split(inds, test_size=1 - train_ratio)
     val, test = train_test_split(test, test_size=test_ratio/(test_ratio + validation_ratio))
 
     return train, val, test
 
-def tokenize(dataset, target, label_process):
-    labels = {label_process(p[target]) for p in dataset.proteins}
+def tokenize(dataset, target, level, label_process):
+    proteins, _ = dataset.proteins()
+    labels = {label_process(p[level][target]) for p in proteins}
     return {label: i for i, label  in enumerate(labels)}
 
 def get_task_info(dataset):
@@ -34,8 +36,9 @@ def get_task_info(dataset):
     data_name = dataset.__class__.__name__
 
     label_processors = {
-                        'ECDataset': lambda label: label.split(".")[0]
-                    }
+                        'EnzymeCommissionDataset': lambda label: label.split(".")[0],
+                        'TMScoreDataset': lambda label: label
+                        }
 
     cache_dir = '.proteinshake'
     if not osp.exists(cache_dir):
@@ -48,11 +51,19 @@ def get_task_info(dataset):
         print(">>> Cached task info not found, computing it.")
 
 
-        if data_name == 'ECDataset':
+        if data_name == 'EnzymeCommissionDataset':
             task_type = 'multi-class'
             target = 'EC'
-            token_map = tokenize(dataset, target, label_processors[data_name])
+            level = 'protein'
+            token_map = tokenize(dataset, target, level, label_processors[data_name])
             num_labels = len(token_map)
+        elif data_name == 'TMScoreDataset':
+            task_type = 'regression'
+            target = 'tm_score'
+            num_labels = 1
+            pass
+        else:
+            raise ValueError
 
         task_dict = {'task_type': task_type,
                     'target': target,
@@ -69,12 +80,12 @@ def get_task_info(dataset):
     task_dict['label_processor'] = label_processors[data_name]
     return task_dict
 
-def add_y_pyg(pyg_dataset, ps_dataset):
+def set_y_pyg(pyg_dataset, ps_dataset):
     task_dict = get_task_info(ps_dataset)
     for p in pyg_dataset:
         label_processor = task_dict['label_processor']
         target = task_dict['target']
-        p.y = task_dict['token_map'][label_processor(getattr(p, target))]
+        p.y = task_dict['token_map'][label_processor(getattr(p, target)[0])]
 
 def get_evaluator(ps_dataset):
     """ Returns scikit metric for dataset.
@@ -92,38 +103,17 @@ def get_splits(ps_dataset):
             'test': task_dict['test_ind']
             }
 
-    pass
 if __name__ == "__main__":
     import torch
     from torch_geometric.data import DataLoader
     from torch_geometric.nn import GraphConv
-    from proteinshake.datasets import ECDataset
+    from proteinshake.datasets import EnzymeCommissionDataset
 
-    d = ECDataset()
+    d = EnzymeCommissionDataset()
     d_pyg = d.to_graph(eps=3).pyg()
 
     task_dict = get_task_info(d)
 
-    add_y_pyg(d_pyg, d)
+    set_y_pyg(d_pyg, d)
     evaluator = get_evaluator(d)
     splits = get_splits(d)
-
-    loader_train = DataLoader(d_pyg)
-
-    class Model(torch.nn.Module):
-        def __init__(self, in_dim, n_classes, hidden_dim=32):
-            self.conv_1 = GraphConv(in_dim, hidden_dim)
-            self.out = torch.nn.Softmax(hidden_dim, n_classes)
-
-        def forward(self, x, edge_index):
-            h = self.conv_1(x, edge_index)
-            h = self.out(h)
-            pass
-
-    model = Model(d_pyg[0].x.shape[1], task_dict['num_classes'])
-
-    for g in loader_train:
-        out = model(g.edge_index, g.x)
-        print(evaluator(out, g.y))
-    pass
-
