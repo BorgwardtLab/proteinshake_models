23512889914910a8629419f072558f6efd52621e
claying <dexiongc@gmail.com>
2023-03-29 16:15:15 +0200

    clean up

--- debug.py
+++
@@ -1,8 +0,0 @@
-from proteinshake.datasets import EnzymeCommissionDataset
-from proteinshake.tasks import *
-
-task = RetrieveTask(root='data/tm')
-ds = task.dataset.to_graph(eps=5).pyg()
-
-for (graph1, protein_dict1), (graph2, protein_dict2)  in ds[task.train_ind[:5]]:
-    print(task.target(protein_dict1, protein_dict2))
--- submit.sh
+++
@@ -1,3 +0,0 @@
-sbatch --cpus-per-task 2 -p gpu --gres=gpu:1 --mem-per-cpu 30G --output="$SCRIPTPATH/logs/$EMB.log" --wrap="python -m src.train --task PT --epochs 10 --representation voxel --batchsize 12"
-
-sbatch --cpus-per-task 2 -p gpu --gres=gpu:1 --mem-per-cpu 30G --output="$SCRIPTPATH/logs/$EMB.log" --wrap="python -m src.train --task PT --epochs 10 --representation point --batchsize 32"
