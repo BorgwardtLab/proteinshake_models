e04519de9991b7f1ad5373266d0ba2223f8ea629
claying <dexiongc@gmail.com>
2023-04-04 17:10:29 +0200

    update array id

--- job_scripts/submit_train.sh
+++ job_scripts/submit_train.sh
@@ -2,11 +2,12 @@
 #SBATCH --job-name="proteinshake_eval"
 #SBATCH --cpus-per-task=5
 #SBATCH --mem-per-cpu=5G
-#SBATCH --gpus-per-node=h100_pcie_1g.20gb:1
+#SBATCH --gpus-per-node=h100_pcie_2g.20gb:1
 #SBATCH --exclude=hpcl9101
 #SBATCH --time=10:00:00
 #SBATCH --partition=p.hpcl91
 #SBATCH --output slurm_logs/train_%A_%a.log
+#SBATCH --array=0-3
 
 
 hostname; date
@@ -16,6 +17,6 @@ mamba activate shakesat
 
 cd ..
 
-python experiments/train.py -m task=enzyme_class representation=graph seed=0,1,2,3
+python experiments/train.py -m task=enzyme_class representation=graph seed=${SLURM_ARRAY_TASK_ID}
 
 exit 0
